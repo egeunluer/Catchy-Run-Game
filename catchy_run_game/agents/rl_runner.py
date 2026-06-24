6a4ae7c96a550dc299d6707c991a4122bbf02e0e
egeunluer <ege.unluer18@gmail.com>
2026-06-24 16:21:19 +0200

    Small adjustment in rl_runner.py

--- catchy_run_game/agents/rl_runner.py
+++ catchy_run_game/agents/rl_runner.py
@@ -14,7 +14,7 @@ from .. import engine
 
 _PACKAGE_ROOT = Path(__file__).resolve().parents[2]
 _REPO_ROOT = _PACKAGE_ROOT.parent
-DEFAULT_MODEL_PATH = _REPO_ROOT / "catchy_run/trained_model_checkpoints/runner_models/catchy_run_runner_stage0_v1_4_1.zip"
+DEFAULT_MODEL_PATH = _REPO_ROOT / "trained_model_checkpoints/runner_models/catchy_run_runner_stage0_v1_4_1.zip"
 
 _model = None
 
